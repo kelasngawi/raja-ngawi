1f7b04b1ec669bcbe5fe32067a471a95c9dd9fc5
ade putra <2511500078@atmaluhur.ac.id>
2026-03-16 13:23:22 +0700

    update semua file

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "tabel_siswa";
+
+$koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if(!$koneksi){
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
+
+?>
